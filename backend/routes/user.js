8a4ec2b6bcffe80ea839b1630dc3510b6dda05fc
asdacosta <asdacosta18@gmail.com>
2024-11-04 22:57:59 +0000

    Define routes for user

---
+++ backend/routes/user.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+  createPost,
+  createUser,
+  deletePost,
+  getAllData,
+  getComments,
+  getPost,
+  getPosts,
+  getUser,
+  updatePost,
+} from "../controllers/userControl.js";
+
+const userRoutes = express.Router();
+
+userRoutes.get("/:userId", getUser);
+userRoutes.get("/:userId/posts/", getPosts);
+userRoutes.get("/:userId/posts/:postId", getPost);
+userRoutes.get("/:userId/posts/:postId/comments", getComments);
+userRoutes.get("/all", getAllData);
+userRoutes.post("/", createUser);
+userRoutes.post("/:userId/post", createPost);
+userRoutes.put("/:userId/post/:postId", updatePost);
+userRoutes.delete("/:userId/post/:postId", deletePost);
+
+export { userRoutes };
