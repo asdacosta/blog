6f7fc436ac430ae7d369319203ba95764d71237c
asdacosta <asdacosta18@gmail.com>
2024-11-05 06:08:00 +0000

    Use passport to set up log in and set up sign up, log out

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,14 @@ import { PrismaClient } from "@prisma/client";
 import { userRoutes } from "./routes/user.js";
 import { postRoutes } from "./routes/post.js";
 import { genRoutes } from "./routes/general.js";
+import {
+  localStrategy,
+  deserialize,
+  signUpValidation,
+  postSignUp,
+  getLogOut,
+} from "./controllers/genControl.js";
+import passport from "passport";
 
 dotenv.config();
 const app = express();
@@ -11,10 +19,24 @@ const prisma = new PrismaClient();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json);
+app.use(passport.session());
+passport.use(localStrategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser(deserialize);
 
 app.use("/user", userRoutes);
 app.use("/post", postRoutes);
 app.use("/", genRoutes);
+
+app.get("/log-out", getLogOut);
+app.post("/sign-up", signUpValidation, postSignUp);
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/log-in",
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
