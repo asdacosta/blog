610f92ddf06f9f1d55cace5630936eef77d3f301
asdacosta <asdacosta18@gmail.com>
2024-11-07 21:37:13 +0000

    Define navigation

---
+++ users-frontend/src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+function Nav() {
+  return (
+    <nav>
+      <h3>Blog</h3>
+      <section>
+        <button>Home</button>
+        <button>Sign In</button>
+        <button>Sign Up</button>
+      </section>
+    </nav>
+  );
+}
+
+export { Nav };
