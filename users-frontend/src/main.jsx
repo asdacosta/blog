3556f13c3854ea6b85a5e38cba64d6725fbf3b08
asdacosta <asdacosta18@gmail.com>
2024-11-07 21:32:56 +0000

    Define routes

--- users-frontend/src/main.jsx
+++ users-frontend/src/main.jsx
@@ -1,9 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Main } from "./components/Main/Main.jsx";
+import { Article } from "./components/Article/Article.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Main /> },
+      { path: "article/:id", element: <Article /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
