19df202e7c1302a632726615ceb1d3e503a86ce1
asdacosta <asdacosta18@gmail.com>
2024-11-11 21:18:39 +0000

    Fetch data and display

--- users-frontend/src/components/Article/Article.jsx
+++ users-frontend/src/components/Article/Article.jsx
@@ -1,17 +1,39 @@
 import { useParams } from "react-router-dom";
 import { content } from "../Main/content";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Article() {
+  const [loading, setLoading] = useState(true);
+  const [article, setArticle] = useState({});
+  const [error, setError] = useState(null);
   const { id } = useParams();
-  const article = content[id - 1];
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get(`/post/${id}`);
+        setArticle(response.data);
+        console.log("Here's it: ", response.data);
+      } catch (error) {
+        setError("Failed to fetch article");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchArticles();
+  }, [id]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+  if (!article) return <p>Article not found</p>;
 
   return (
     <section className="article">
-      <h1>{article.header}</h1>
+      <h1>{article.title}</h1>
       <section>
-        {article.text.map((paragraph, index) => (
-          <p key={index}>{paragraph}</p>
-        ))}
+        <p>{article.content}</p>
       </section>
       <section className="comment">
         <h2>Comments</h2>
@@ -20,13 +42,20 @@ function Article() {
           <input type="text" placeholder="Add comment..." name="commentText" />
           <button>Add</button>
         </form>
-        {article.comments.map((comment, index) => (
-          <div key={index}>
-            <h3>{comment.user}</h3>
-            <span>{comment.comment}</span>
-            <span>{comment.date}</span>
-          </div>
-        ))}
+        {article.comments.map((comment, index) => {
+          const responseDate = new Date(comment.createdAt);
+          const formattedDate = responseDate.toLocaleDateString();
+          const formattedTime = responseDate.toLocaleTimeString();
+          return (
+            <div key={index}>
+              <h3>{comment.name}</h3>
+              <span>{comment.content}</span>
+              <span>
+                {formattedDate} {formattedTime}
+              </span>
+            </div>
+          );
+        })}
       </section>
     </section>
   );
