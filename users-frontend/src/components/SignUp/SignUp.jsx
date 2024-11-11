574bc16d7986f76a9fd7134dcb8895c183986463
asdacosta <asdacosta18@gmail.com>
2024-11-11 21:19:04 +0000

    Clear trivial name inputs

--- users-frontend/src/components/SignUp/SignUp.jsx
+++ users-frontend/src/components/SignUp/SignUp.jsx
@@ -3,14 +3,6 @@ function SignUp() {
     <section className="signForm">
       <form action="">
         <div>
-          <label htmlFor="first-name">First Name</label>
-          <input type="text" name="first-name" id="first-name" required />
-        </div>
-        <div>
-          <label htmlFor="last-name">Last Name</label>
-          <input type="text" name="last-name" id="last-name" required />
-        </div>
-        <div>
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" required />
         </div>
