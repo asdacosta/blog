8de27fdb35cfea05dafaf9330d77c5433944536d
asdacosta <asdacosta18@gmail.com>
2024-11-12 23:18:17 +0000

    Define route to add comment

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -5,11 +5,13 @@ import {
   getPost,
   updateComment,
 } from "../controllers/postControl.js";
+import { createComment } from "../models/genModel.js";
 
 const postRoutes = express.Router();
 const prisma = new PrismaClient();
 
 postRoutes.get("/:postId", getPost);
+postRoutes.post("/:postId/newComment", createComment);
 postRoutes.put("/:postId/comment/:commentId", updateComment);
 postRoutes.delete("/:postId/comment/:commentId", deleteComment);
 
