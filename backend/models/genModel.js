8d526630fcd4281bac2e0f7859f502ff1e7921f9
asdacosta <asdacosta18@gmail.com>
2024-11-12 23:18:06 +0000

    Redefine function to enable use of req and res

--- backend/models/genModel.js
+++ backend/models/genModel.js
@@ -1,12 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const createComment = async (postId, name, content) => {
+const createComment = async (req, res) => {
   try {
-    const postIdInt = parseInt(postId);
-    return await prisma.comment.create({
-      data: { content, postId: postIdInt, name },
+    const postIdInt = parseInt(req.params.postId);
+    const { name, content } = req.body;
+    if (!name || !content) {
+      return res.status(400).json({ error: "Name and content are required" });
+    }
+
+    const newComment = await prisma.comment.create({
+      data: {
+        content,
+        postId: postIdInt,
+        name,
+      },
     });
+    res.json({ message: "Comment added successfully" });
   } catch (error) {
     console.error("Error creating comment:", error);
     throw error;
