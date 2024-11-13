8207546b150b0a740dc1855ebeca0fdab9b08dbf
asdacosta <asdacosta18@gmail.com>
2024-11-13 16:48:24 +0000

    Log in using React instead of backend

--- users-frontend/src/components/SignIn/SignIn.jsx
+++ users-frontend/src/components/SignIn/SignIn.jsx
@@ -1,16 +1,50 @@
+import axios from "axios";
+import { useState } from "react";
+
 function SignIn() {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async (e) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("/api/log-in", { email, pwd });
+      localStorage.setItem("token", response.data.token);
+      window.location.href = "/";
+    } catch (error) {
+      setError("Invalid email or password. Please try again.");
+      console.error("Login failed:", error);
+    }
+  };
+
   return (
     <section className="signForm">
-      <form action="">
+      <form onSubmit={handleLogin}>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" required />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
         <div>
           <label htmlFor="pwd">Password</label>
-          <input type="password" name="pwd" id="pwd" required />
+          <input
+            type="password"
+            name="pwd"
+            id="pwd"
+            value={pwd}
+            onChange={(e) => setPwd(e.target.value)}
+            required
+          />
         </div>
         <button>Sign In</button>
+        {error && <p className="error">{error}</p>}
       </form>
     </section>
   );
