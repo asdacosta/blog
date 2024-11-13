6e87bad354b0d873315b8a9af339a58cd057353c
asdacosta <asdacosta18@gmail.com>
2024-11-13 00:00:17 +0000

    Retrieve post data from server and use instead of dummy content

--- users-frontend/src/components/Main/Main.jsx
+++ users-frontend/src/components/Main/Main.jsx
@@ -1,24 +1,46 @@
 import { Link } from "react-router-dom";
-import { content } from "./content";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import blogTemplate from "../../assets/blog.jpg";
 
 function Main() {
+  const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get("/api");
+        console.log("That response: ", response.data);
+        setArticles(response.data);
+      } catch (error) {
+        setError("Failed to fetch article");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
   return (
     <main>
       <h1>Everything Coding</h1>
       <section className="cardsBox">
-        {content.map((card, index) => (
-          <section key={card.alt}>
-            <img src={card.img} alt={card.alt} />
+        {articles.map((article, index) => (
+          <section key={index}>
+            <img src={blogTemplate} alt="Blog Template" />
             <div>
-              <h4>{card.header}</h4>
+              <h4>{article.title}</h4>
               <div className="cardText">
                 <span>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                     <path d="M160 368c26.5 0 48 21.5 48 48l0 16 72.5-54.4c8.3-6.2 18.4-9.6 28.8-9.6L448 368c8.8 0 16-7.2 16-16l0-288c0-8.8-7.2-16-16-16L64 48c-8.8 0-16 7.2-16 16l0 288c0 8.8 7.2 16 16 16l96 0zm48 124l-.2 .2-5.1 3.8-17.1 12.8c-4.8 3.6-11.3 4.2-16.8 1.5s-8.8-8.2-8.8-14.3l0-21.3 0-6.4 0-.3 0-4 0-48-48 0-48 0c-35.3 0-64-28.7-64-64L0 64C0 28.7 28.7 0 64 0L448 0c35.3 0 64 28.7 64 64l0 288c0 35.3-28.7 64-64 64l-138.7 0L208 492z" />
                   </svg>{" "}
-                  {card.commentNum}
+                  {article.comments.length}
                 </span>
-                <Link to={card.link}>
+                <Link to={`/post/${article.id}`}>
                   <span>
                     Read{" "}
                     <svg
