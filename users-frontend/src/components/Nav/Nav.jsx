b342f97e724aea5263c0133487283de71ed63319
asdacosta <asdacosta18@gmail.com>
2024-11-14 15:34:54 +0000

    Display respective links depending on if user is logged in or out

--- users-frontend/src/components/Nav/Nav.jsx
+++ users-frontend/src/components/Nav/Nav.jsx
@@ -1,21 +1,40 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Nav() {
+  const [token, setToken] = useState(false);
+
+  useEffect(() => {
+    const loggedIn = !!localStorage.getItem("token");
+    setToken(loggedIn);
+  }, []);
+
+  const handleLogOut = () => {
+    localStorage.removeItem("token");
+    setToken(false);
+  };
+
   return (
     <nav>
       <Link to="/">
         <h3>Blog</h3>
       </Link>
       <section>
-        <Link to="/">
-          <button>Home</button>
-        </Link>
-        <Link to="/sign-in">
-          <button>Sign In</button>
-        </Link>
-        <Link to="/sign-up">
-          <button>Sign Up</button>
-        </Link>
+        {token ? (
+          <>
+            <Link to="/">
+              <button>Home</button>
+            </Link>
+            <Link to="/sign-in">
+              <button>Sign In</button>
+            </Link>
+            <Link to="/sign-up">
+              <button>Sign Up</button>
+            </Link>
+          </>
+        ) : (
+          <button onClick={handleLogOut}>Log Out</button>
+        )}
       </section>
     </nav>
   );
