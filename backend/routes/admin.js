9136589c94a6fd6c3d80189b60f749ea90d7685f
asdacosta <asdacosta18@gmail.com>
2024-11-16 22:58:01 +0000

    Add routes for publish, unpublish and delete of comment

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { createPost } from "../controllers/userControl.js";
+import {
+  createPost,
+  deleteComment,
+  publishPost,
+  unpublishPost,
+} from "../controllers/userControl.js";
 
 const adminRoutes = express.Router();
 adminRoutes.post("/post/:userId", createPost);
+adminRoutes.post("/published/unpublish/:id", unpublishPost);
+adminRoutes.post("/unpublished/publish/:id", publishPost);
+adminRoutes.delete("/comments/:articleId/:commentId", deleteComment);
 
 export { adminRoutes };
