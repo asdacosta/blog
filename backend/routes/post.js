29077386283295a7848d39509fb9ceb71f4cacd5
asdacosta <asdacosta18@gmail.com>
2024-11-16 11:25:26 +0000

    Add routes to get published and unpublished posts

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -3,6 +3,8 @@ import { PrismaClient } from "@prisma/client";
 import {
   deleteComment,
   getPost,
+  getPublishedPosts,
+  getUnpublishedPosts,
   updateComment,
 } from "../controllers/postControl.js";
 import { createComment } from "../models/genModel.js";
@@ -10,6 +12,8 @@ import { createComment } from "../models/genModel.js";
 const postRoutes = express.Router();
 const prisma = new PrismaClient();
 
+postRoutes.get("/published", getPublishedPosts);
+postRoutes.get("/unpublished", getUnpublishedPosts);
 postRoutes.get("/:postId", getPost);
 postRoutes.post("/:postId/newComment", createComment);
 postRoutes.put("/:postId/comment/:commentId", updateComment);
