e1eb91757d29f7098667938c3aa7936f98211fd2
asdacosta <asdacosta18@gmail.com>
2024-11-16 11:16:25 +0000

    Fetch all posts and display their comments

--- admin-frontend/src/components/Comments/Comments.jsx
+++ admin-frontend/src/components/Comments/Comments.jsx
@@ -1,20 +1,72 @@
-import { content } from "./content";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Comments() {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get("/api");
+        setArticles(response.data);
+      } catch (error) {
+        setError("Failed to fetch articles");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  const handleDelete = async (articleId, commentId) => {
+    try {
+      const response = await axios.delete(`/api/post/comments/${id}`);
+      if (response.status === 201 || response.status === 200) {
+        setArticles((prevArticles) =>
+          prevArticles.map((article) =>
+            article.id === articleId
+              ? {
+                  ...article,
+                  comments: article.comments.filter(
+                    (comment) => comment.id !== commentId
+                  ),
+                }
+              : article
+          )
+        );
+      }
+    } catch (error) {
+      setError("Failed to unpublish post");
+      console.error(error);
+    }
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
     <section>
-      {content.map((article, index) => {
-        <div key={index}>
-          <span>User: {article.header}</span>
-          {article.comments.map((comment) => {
-            <div>
-              <span>Username: {comment.user}</span>
-              <span>Comment: {comment.comment}</span>
-              <button>Delete</button>
-            </div>;
-          })}
-        </div>;
-      })}
+      {articles.map((article) => (
+        <div key={article.id} className="postBox">
+          <span>Title: {article.title}</span>
+          {article.comments.length > 0 ? (
+            article.comments.map((comment) => (
+              <div key={comment.id} className="post">
+                <span>Username: {comment.name}</span>
+                <span>Comment: {comment.content}</span>
+                <button onClick={() => handleDelete(article.id, comment.id)}>
+                  Delete
+                </button>
+              </div>
+            ))
+          ) : (
+            <span>No Comments</span>
+          )}
+        </div>
+      ))}
     </section>
   );
 }
