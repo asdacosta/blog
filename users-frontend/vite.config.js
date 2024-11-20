bed2a6c6ede0d38ac197e6042c087cd595c12191
asdacosta <asdacosta18@gmail.com>
2024-11-20 22:36:41 +0000

    Replace local server link with deployed one

--- users-frontend/vite.config.js
+++ users-frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://blog-server-bpfu.onrender.com/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
